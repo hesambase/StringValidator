38dee6d81afbc1c52e54078e299cfd91c3f77a96
hesam attari <Hesam_att@outlook.com>
2022-11-23 12:23:58 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using StringValidator;
+
+Solution _solution = new Solution();
+string text = "()";
+Console.WriteLine("Input string: " + text);
+Console.WriteLine("Validation of string: " + _solution.IsValid(text));
+text = "()[]{}";
+Console.WriteLine("Input string: " + text);
+Console.WriteLine("Validation of string: " + _solution.IsValid(text));
+text = "[()[]{}]";
+Console.WriteLine("Input string: " + text);
+Console.WriteLine("Validation of string: " + _solution.IsValid(text));
+text = "[()";
+Console.WriteLine("Input string: " + text);
+Console.WriteLine("Validation of string: " + _solution.IsValid(text));
+text = "[)";
+Console.WriteLine("Input string: " + text);
+Console.WriteLine("Validation of string: " + _solution.IsValid(text));
---
+++ Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringValidator
+{
+    public class Solution
+    {
+        public bool IsValid(string s)
+        {
+            if (s.Length > Math.Pow(10, 4))//constraint
+                return false;
+                Stack<char> ch = new Stack<char>();
+                foreach (var item in s.ToCharArray())
+                    if (item == '(')
+                        ch.Push(')');
+                    else if (item == '[')
+                        ch.Push(']');
+                    else if (item == '{')
+                        ch.Push('}');
+                    else if (ch.Count == 0 || ch.Pop() != item)
+                        return false;
+
+                return ch.Count == 0;
+            
+        }
+    }
+}
